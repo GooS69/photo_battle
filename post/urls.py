e86164aa9eb5c933b9625a17a3af99fae3bcd92b
GooS69 <guseff.daniil2011@yandex.ru>
2021-06-02 14:14:22 +0300

    first view

--- post/urls.py
+++ post/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
                ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def index(request):
     return HttpResponse("Hello, world")
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/detail.html', context={'post':post})
