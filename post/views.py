1d4951308d6e1c1cda73654e9a708a2d2edd2ca7
GooS69 <guseff.daniil2011@yandex.ru>
2021-06-09 16:17:27 +0300

    edit views

--- post/views.py
+++ post/views.py
@@ -4,12 +4,15 @@ from .models import Post, Comment, User
 
 
 def index(request):
-    posts = get_list_or_404(Post)
-    return render(request, 'post/index.html', context={'posts':posts})
+    posts = Post.objects.all()
+    if not posts:
+        return HttpResponse("<H3>No posts</H3>")
+    else:
+        return render(request, 'post/index.html', context={'posts':posts})
 
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    comments = get_list_or_404(Comment,parent=post)
+    comments = Comment.objects.filter(parent_id=post_id)
 
     return render(request, 'post/detail.html', context={'post':post, 'comments' : comments})
