d984675b454c97d359bbed31b0802e9ea45dd620
GooS69 <guseff.daniil2011@yandex.ru>
2021-06-19 16:30:28 +0300

    edit admin

--- post/admin.py
+++ post/admin.py
@@ -5,6 +5,16 @@ from .models.post import Post
 from .models.like import Like
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Like)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
