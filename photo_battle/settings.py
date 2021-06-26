269d6d2354f12f5883c8ae41612736af06f963ea
GooS69 <guseff.daniil2011@yandex.ru>
2021-06-26 14:28:27 +0300

    simple db logging

--- photo_battle/settings.py
+++ photo_battle/settings.py
@@ -167,3 +167,33 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': 'debug.log'
+        }
+    },
+    'loggers': {
+        'django.db.backends':{
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        }
+    }
+}
