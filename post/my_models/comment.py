a0bf71da8746a2d19a4995cea8e7e5d7fdf6eae2
GooS69 <guseff.daniil2011@yandex.ru>
2021-07-11 12:45:34 +0300

    fix number of comments

--- post/my_models/comment.py
+++ post/my_models/comment.py
@@ -13,7 +13,7 @@ class Comment(models.Model):
 	#post = my_models.ForeignKey('Post', on_delete=my_models.CASCADE, related_name='comments', related_query_name='comment')
 	#parent = my_models.ForeignKey('self', on_delete=my_models.CASCADE, null=True, default=None, blank=True, related_name='comments', related_query_name='comment')
 
-	comments = GenericRelation('self')
+	comments = GenericRelation('self', related_query_name='parent')
 
 	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 	object_id = models.PositiveIntegerField()
@@ -28,19 +28,23 @@ class Comment(models.Model):
 		verbose_name_plural = 'Коментарии'
 
 
-#@receiver(pre_save, sender=Comment)
-#def set_new_record_flag(sender, instance, *args, **kwargs):
-#	instance.__new_record = not bool(instance.id)
+@receiver(pre_save, sender=Comment)
+def set_new_record_flag(sender, instance, *args, **kwargs):
+	instance.__new_record = not bool(instance.id)
 
 
-#@receiver(post_save, sender=Comment)
-#def incr_number_of_comments_on_post(sender, instance, *args, **kwargs):
-#	if instance.__new_record:
-#		instance.post.number_of_comments += 1
-#		instance.post.save()
+@receiver(post_save, sender=Comment)
+def incr_number_of_comments_on_post(sender, instance, *args, **kwargs):
+	if instance.__new_record:
+		while type(instance.content_object) == Comment:
+			instance = instance.content_object
+		instance.content_object.number_of_comments += 1
+		instance.content_object.save()
 
 
-#@receiver(post_delete, sender=Comment)
-#def decr_number_of_comments_on_post(sender, instance, *args, **kwargs):
-#	instance.post.number_of_comments -= 1
-#	instance.post.save()
+@receiver(post_delete, sender=Comment)
+def decr_number_of_comments_on_post(sender, instance, *args, **kwargs):
+	while type(instance.content_object) == Comment:
+		instance = instance.content_object
+	instance.content_object.number_of_comments -= 1
+	instance.content_object.save()
