6012c5eafb3bfc78148e251507588e6b70de7d48
GooS69 <guseff.daniil2011@yandex.ru>
2021-07-15 00:31:33 +0300

    ajax delete post

--- post/my_views/delete_post.py
+++ post/my_views/delete_post.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.urls import reverse_lazy
-from django.views.generic import DeleteView
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import DeletionMixin
 
 from post.my_models.post import Post
 
 
-class DeletePost(UserPassesTestMixin, DeleteView):
+class DeletePost(UserPassesTestMixin, DeletionMixin, SingleObjectMixin,View):
     model = Post
-    template_name = 'post/delete_post.html'
 
-    def get_success_url(self):
-        self.success_url = reverse_lazy('post:user_page', args=[self.request.user.id])
-        return super().get_success_url()
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return HttpResponse(status=200)
 
     def test_func(self):
         return self.request.user == self.get_object().owner
