799a7ba00346e06ce33eea61f9aa3f17bbd67c81
GooS69 <guseff.daniil2011@yandex.ru>
2021-07-19 10:43:50 +0300

    async pagination for muser page

--- post/my_views/user_view.py
+++ post/my_views/user_view.py
@@ -50,3 +50,8 @@ class UserPosts(UserPassesTestMixin, ListView):
 
     def test_func(self):
         return self.request.user.id == self.kwargs['pk']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status'] = self.kwargs['status']
+        return context
