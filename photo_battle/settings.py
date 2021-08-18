624178c3278472969ab095100301d805251371a8
GooS69 <guseff.daniil2011@yandex.ru>
2021-08-18 16:19:04 +0300

    solve override first name after logout

--- photo_battle/settings.py
+++ photo_battle/settings.py
@@ -193,7 +193,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
+    'post.my_pipeline.custom_user_details.custom_user_details'
+    #'social_core.pipeline.user.user_details',
 )
 
 LOGGING = {
