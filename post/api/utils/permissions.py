c180b51efa474a42289d995c05ac27bb9cfe16b8
GooS69 <guseff.daniil2011@yandex.ru>
2021-08-27 02:31:40 +0300

    fix post permissions

--- post/api/utils/permissions.py
+++ post/api/utils/permissions.py
@@ -3,9 +3,6 @@ from rest_framework import permissions
 
 class PostsPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'POST':
-            return bool(request.user and request.user.is_authenticated)
-
-        if request.query_params.get('preset') == 'user_page':
+        if request.method == 'POST' or request.query_params.get('preset') == 'user_page':
             return bool(request.user and request.user.is_authenticated)
         return True
